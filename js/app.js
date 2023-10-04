ffe84453f834b5e156d05148058b394dce423872
Agussantos84704 <124318393+Agussantos84704@users.noreply.github.com>
2023-10-03 21:48:04 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let numero = 0;  // Se utiliza para saber el nº de pokemon a adivinar
+let cantidadAcertados = 0;  // Se utiliza para saber cuantos pokemons fueron adivinados
+let cantidadEncuestados = 0;  // Se utiliza para saber cuantos pokemons se intentaron adivinar
+
+// Alumno: crear la función restart:
+
+// 1) Debe borrar el contenido de texto de "resultText"
+// 2) Debe quitar la clase "success" de la imagen "imgpokemon"
+// 3) Debe crear un número aleatorio de 0 a la cantidad de pokemons
+// disponibles en la variable "pokemons" (proveniente del archivo data.js)
+// Esa variable la debe guardar en "numero".
+// 5) Debe modificar el atributo de "src" de "imgpokemon" para
+// que muestre como imagen el "thumbnail" del nº de pokemon a adivinar
+// (utilice la variable "numero" para buscar el pokemon candidato en "pokemons")
+
+// function restart
+
+function restart (){
+    document.getElementById("resultText").textContent = "";
+    document.getElementById("imgpokemon").classList.remove("success");
+    numero = Math.floor(Math.random() * (pokemons.length));
+    let pokeImage = pokemons[numero]
+    document.getElementById("imgpokemon").src = pokeImage.thumbnail;
+    document.querySelector('#skills-description').setAttribute('style', 'display: none');
+    clearSkills();
+    input.value = "";
+
+}
+
+agregarEventos();
+restart();
